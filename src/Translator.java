517f8800652c02099d7e3015ba13956b058e3d14
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-11-01 09:59:24 -0400

    Loops for words to translate
    if a word starts with "a,e,i,o,u"
    if a word ends with "a,e,i,o,u"

---
+++ src/Translator.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author baayl3669
+ */
+public class Translator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create the input for the user
+        Scanner in = new Scanner(System.in);
+
+        // make a string for the word
+        String word = "";
+
+        // make a while loop to ask for words to translate
+        while (!word.equals("END")) {
+            System.out.println("Please enter a word to translate(Type END to quit the program): ");
+            word = in.nextLine();
+            // make a statement if a word STARTS with a vowel
+            if (word.startsWith("a")
+                    || word.startsWith("e")
+                    || word.startsWith("i")
+                    || word.startsWith("o")
+                    || word.startsWith("u")) {
+                String latinvowel = word.replace("" + word.charAt(0), "i");
+
+                // make a stetement if ENDS with a vowel
+                if (word.endsWith("a")
+                        || word.endsWith("e")
+                        || word.endsWith("i")
+                        || word.endsWith("o")
+                        || word.endsWith("u")
+                        || word.endsWith("y")) {
+                    latinvowel = latinvowel + "hee";
+
+                    }
+                }
+            }
+        }
+    }
+
