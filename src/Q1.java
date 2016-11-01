68f566715c62031d8b810bb19d63bab5cdf17fa6
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-11-01 10:08:31 -0400

    11/1. Created project.

---
+++ src/Q1.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schum0689
+ */
+public class Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Create scanner for user input
+        Scanner input = new Scanner(System.in);
+        // Ask user for a word to translate
+        System.out.println("Translate:");
+        // Get english word to translate
+    }
+}
