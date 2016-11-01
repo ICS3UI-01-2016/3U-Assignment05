bdd20c791dddae6baaafbf99a9829eb91e807dd0
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-11-01 10:05:33 -0400

    A5

---
+++ src/MattenenglischTranslator.java
@@ -0,0 +1,71 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author manok5757
+ */
+public class MattenenglischTranslator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner for input
+        Scanner input = new Scanner(System.in);
+        // ask the user for input
+        System.out.println("Please enter a word to translate(Type END to quit the program): ");
+        // scan in a word
+        String word = input.nextLine();
+
+        // make sure the input is lowercase
+        word = word.toLowerCase();
+        // make the program loop until they press end
+        while (!word.equalsIgnoreCase("END")) {
+            // make changes to a word if it begins with a constant
+            if (!word.startsWith("a") || !word.startsWith("e") || !word.startsWith("i")
+                    || !word.startsWith("o") || !word.startsWith("u") || (word.length() > 1 && !word.startsWith("y"))) {
+
+                // create variable for changed word 
+                
+                vowelPosition = word.indexOf("a" );() "e" || "i" || "o" || "u" || "y");
+                if (vowelPosition == 0) {
+                    
+                }
+                
+                
+            
+        
+    
+
+){
+                
+                
+                
+                
+             }   
+            }
+            
+            
+        
+    
+
+
+                
+           
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+        
+    }
+}
