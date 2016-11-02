46008e88286d32f029a73a2d42a019f4f343a040
gavra1870 <gavra1870@HRH-IC0027342.wrdsb.ca>
2016-11-02 09:39:37 -0400

    Assignment 5 hw

---
+++ src/A5Q1.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gavra1870
+ */
+public class A5Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Create a scanner for input
+        Scanner input = new Scanner(System.in); 
+        
+        //Scan in the word
+        String vowel = input.nextLine();
+       
+        //convert all of the letters to lower case
+        vowel= vowel.toLowerCase();
+        
+        // walk through the length of the word
+          int length = vowel.length();
+          int count = 0;
+        //
+    }
+}
