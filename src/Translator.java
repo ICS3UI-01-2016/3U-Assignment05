1e7ebcd747c55153ef6cdfa338f4c242d4812f20
fitaa8228 <fitaa8228@HRH-IC0027347.wrdsb.ca>
2016-11-03 09:46:19 -0400

    Completed

--- src/Translator.java
+++ src/Translator.java
@@ -20,30 +20,41 @@ public class Translator {
         // TODO code application logic here
         //Create scanner for user input
         Scanner input = new Scanner(System.in);
-        //Ask the user for a word to translate
-        System.out.println("Please enter a word to translate(Type END to quit the program)");
-        //Accept word to translate
-        String word = input.nextLine();
-        //change the first vowel to ~
-        String vowelCheck = word.toLowerCase().replaceAll("[aeiou]", "~");
-        //find where the ~ is
-        int idk = vowelCheck.indexOf("~");
-        //Split up the word where the ~ would be in the original word
-        String end = word.substring(idk);
-        //Ch
-        end = end.replaceFirst("[aeiou]", "i");
-        String begin = word.substring(0, idk);
-        //Check to see if the original word started and ended with a vowel
-        if (vowelCheck.endsWith("~")&&vowelCheck.startsWith("~")) {
-            begin = begin + "hee";
-        } else {
-            begin = begin + "ee";
+        while (true) {
+            //Ask the user for a word to translate
+            System.out.println("Please enter a word to translate(Type END to quit the program)");
+            //Accept word to translate
+            String word = input.nextLine();
+            //if user inputs END end the program
+            if (word.equals("END")) {
+                break;
+            }
+            //change the first vowel to ~
+            String vowelCheck = word.toLowerCase().replaceAll("[aeiou]", "~");
+            //change all y's to `
+            String yWordCheck = word.toLowerCase().replaceAll("y", "=");
+           //Check to see if a y could be a vowel
+            int yCheck = yWordCheck.indexOf("=");
+            if (yCheck > 0) {
+                vowelCheck = word.toLowerCase().replaceAll("y", "~");
+            }
+            //Check to see where the first vowel is
+            int vowel = vowelCheck.indexOf("~");
+            //Split up the word where the ~ would be in the original word
+            String end = word.substring(vowel);
+            //replace the first vowel with an i
+            end = end.replaceFirst("[aeiouy]", "i");
+            String begin = word.substring(0, vowel);
+            //Check to see if the original word started and ended with a vowel
+            if (vowelCheck.endsWith("~") && vowelCheck.startsWith("~")) {
+                begin = begin + "hee";
+            } else {
+                begin = begin + "ee";
+            }
+            //Make the finished word
+            String finished = end + begin;
+            //Show the user the translation
+            System.out.println(word + " in Mattenenglisch is " + finished.toLowerCase());
         }
-        String finished = end + begin;
-        System.out.println(vowelCheck);
-        System.out.println(idk);
-        System.out.println(end);
-        System.out.println(begin);
-        System.out.println(finished);
     }
 }
